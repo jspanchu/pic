111c0182c4c0f1f17127831a1916c930c30c35cf
jaswantp <jaswantphysx@gmail.com>
2018-10-18 13:03:30 +0530

    moved file writes into iterator loop

--- driver.cpp
+++ driver.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 #include "include/num_density.hpp"
 #include "include/vel_dist.hpp"
 #include "include/poisson_solver.hpp"
+#include "include/particle_mover.hpp"
+#include "include/file_io.hpp"
+#include "fstream"
 
+void calcElecField(NumDensity*, VelDist*, PoissonSolver*);
 int main(int argc, char **argv) {
     VelDist *pCharges;
     VelDist counterStream;
@@ -19,27 +24,66 @@ int main(int argc, char **argv) {
 
     pPlasma->setNodes(10000);
     pPlasma->setGridWidth(pCharges->getL());
-    pPlasma->calcElecDensity(pCharges);
-    pPlasma->calcIonDensity(pCharges);
-    pPlasma->calcDensity();
 
     PoissonSolver *pSystem;
     PoissonSolver system(pPlasma,pCharges);
     pSystem = &system;
 
+    ParticleMover* pIterator;
+    ParticleMover iterator;
+    pIterator = &iterator;
+
+    calcElecField(pPlasma, pCharges, pSystem);
+    
+    for (int k = 1; k <= pIterator->getIter(); ++k)
+    {
+        std::cout << "Time step : " << k << std::endl;
+        pIterator->xIncr(pPlasma,pCharges);
+        pIterator->vIncr(pSystem,pCharges);
+        calcElecField(pPlasma, pCharges, pSystem);
+        
+        std::string(j) = std::to_string(k);
+        j.append(".out");
+        FileIO *pPhi;
+        FileIO phi("./phi",j);
+        pPhi = &phi;
+        pPhi->fileWrite("x","phi");
+        for(int i = 0; i < pPlasma->getNodes(); ++i)
+        {
+            pPhi->fileWrite(i, pSystem->getPhi(i));
+        }
+
+        FileIO *pDist;
+        FileIO dist("./fvx",j);
+        pDist = &dist;
+        pDist->fileWrite("x","v","f");
+        for(int i = 0; i < pCharges->getN(); ++i)
+        {
+            pDist->fileWrite(pCharges->getPositionElec(i),pCharges->getV(i),pCharges->getF(i));
+        }
+        FileIO *pDens;
+        FileIO dens("./rho",j);
+        pDens = &dens;
+        pDens->fileWrite("x","rho");
+        for(int i = 0; i < pPlasma->getNodes(); ++i)
+        {
+            pDens->fileWrite(i,pPlasma->getDensity(i));
+        }
+    }
+
+
+
+}
+void calcElecField(NumDensity* pPlasma, VelDist* pCharges, PoissonSolver* pSystem)
+{
+    pPlasma->calcElecDensity(pCharges);
+    pPlasma->calcIonDensity(pCharges);
+    pPlasma->calcDensity();
+
     pSystem->setPhi(pPlasma);
     pSystem->setE();
     pSystem->setLocalE(pPlasma,pCharges);
-	
-    for(int i = 0; i < pPlasma->getNodes(); ++i)
-    {
-        std::cout << i << "," << pSystem->getPhi(i) << std::endl;
-    }
-
 }
-
-
-
 
 
 
