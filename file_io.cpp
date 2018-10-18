9b69a487248445a8169636f994c7c22e0c5b47e5
jaswantp <jaswantphysx@gmail.com>
2018-10-18 13:04:04 +0530

    overloaded to write two or three columns

--- file_io.cpp
+++ file_io.cpp
@@ -1,21 +1,33 @@
 #include "include/file_io.hpp"
 #include <iostream>
-FileIO::FileIO()
+#include <string>
+FileIO::FileIO(std::string filename,std::string index)
 {
 	//Open file for writing and place header row.
-	file.open("./fvx.out", std::ios::trunc | std::ios::out);
+	filename.append(index);
+	this->file.open(filename, std::ios::trunc | std::ios::out);
 }
 FileIO::~FileIO()
 {
-	file.close();
+	this->file.close();
 }
 void FileIO::fileWrite(const char *a, const char *b, const char *c)
 {
 	//write headers
-	file << a << "," << b << "," << c << std::endl;
+	this->file << a << "," << b << "," << c << std::endl;
 }
 void FileIO::fileWrite(double f, double v, double x)
 {
 	//write data
-	file << f << "," << v << "," << x << std::endl;
+	this->file << f << "," << v << "," << x << std::endl;
+}
+void FileIO::fileWrite(const char *a, const char *b)
+{
+	//write headers
+	this->file << a << "," << b << std::endl;
+}
+void FileIO::fileWrite(double f, double v)
+{
+	//write data
+	this->file << f << "," << v << std::endl;
 }
