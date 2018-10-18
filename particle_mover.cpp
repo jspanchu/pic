1b96de267e46959afb246f10b407cc54fa9b9e67
jaswantp <jaswantphysx@gmail.com>
2018-10-18 11:26:42 +0530

    set time according to scale

--- particle_mover.cpp
+++ particle_mover.cpp
@@ -5,14 +5,15 @@
  *      Author: jaswant
  */
 
-#include "particle_mover.hpp"
+#include "include/particle_mover.hpp"
 #include <cmath>
 #include <iostream>
 
 ParticleMover::ParticleMover()
 {
-	t = -2.5e-5; // in terms of 1 / omega = sqrt((m*eps) / (500000*e*e))  ~ 2.506590041516934e-05
-	dt = -2.5e-8; // in terms of 1 / omega = sqrt((m*eps) / (500000*e*e)) ~ 2.506590041516934e-05
+	t = 100; // in terms of 1 / omega = sqrt((m*eps) / (500000*e*e))  ~ 2.506590041516934e-05
+	dt = 0.1; // in terms of 1 / omega = sqrt((m*eps) / (500000*e*e)) ~ 2.506590041516934e-05
+	
 	//ctor
 }
 ParticleMover::~ParticleMover()
@@ -24,32 +25,32 @@ int ParticleMover::getIter()
 {
 	return iter;
 }
-void ParticleMover::setTime(VelDist* pChargeDist, double fac)
+void ParticleMover::setTime(double t)
 {
-	t = fac * sqrt((pChargeDist->getM() * eps) / (pChargeDist->getN()*e*e));
+	this->t = t;
 
 }
-void ParticleMover::setTimeStep(VelDist* pChargeDist, double fac)
+void ParticleMover::setTimeStep(double dt)
 {
-	dt = fac * sqrt((pChargeDist->getM() * eps) / (pChargeDist->getN()*e*e));
+	this->dt = dt;
 }
-void ParticleMover::xIncr(NumDensity* pPlasmaSys, VelDist* pChargeDist)
+void ParticleMover::xIncr(NumDensity* pPlasma, VelDist* pCharges)
 {
 	std::cout << "Pushing all the electrons." << std::endl;
-	for (int i = 0; i < pPlasmaSys->getNumElectrons(); ++i)
+	for (int i = 0; pCharges->getN(); ++i)
 	{
 		//std::cout << i << std::endl;
-		pPlasmaSys->setPositionElec(pPlasmaSys->getPositionElec(i) + dt * (pChargeDist->getV(i)), i);
+		pCharges->setPositionElec(pCharges->getPositionElec(i) + dt * (pCharges->getV(i)), i);
 		//std::cout <<pPlasmaSys->getPositionElec(i) << std::endl;
 	}
 }
-void ParticleMover::vIncr(NumDensity* pPlasmaSys, VelDist* pChargeDist)
+void ParticleMover::vIncr(PoissonSolver* pSystem, VelDist* pCharges)
 {
 	std::cout << "Incrementing velocities of all the electrons." << std::endl;
-	for (int i = 0; i < pPlasmaSys->getNumElectrons(); ++i)
+	for (int i = 0; i < pCharges->getN(); ++i)
 	{
 		//std::cout << i << std::endl;
-		pChargeDist->setV(pChargeDist->getV(i) + dt * e * (pPlasmaSys->getLocalE(i)) / pChargeDist->getM(), i);
+		pCharges->setV(pCharges->getV(i) + dt * (pSystem->getLocalE(i)), i);
 	}
 
 }
